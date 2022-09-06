42aa61d43dc3bc0bcc5652bfc735b69a16027e8c
Runyang Chen <runyang.chen@mediatek.corp-partner.google.com>
2022-09-06 20:10:54 +0800

    soc/mediatek/mt8186: Enable the protection of DEVAPC
    Enable the protection for DEVAPC registers of AO domain.
    
    TEST=build pass.
    BUG=b:244250435
    
    Signed-off-by: Runyang Chen <runyang.chen@mediatek.corp-partner.google.com>
    Change-Id: I8535438d4c7da29c9dcd97be9a2af05ea4690064
    Reviewed-on: https://review.coreboot.org/c/coreboot/+/67434
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yu-Ping Wu <yupingso@google.com>
    Tested-by: build bot (Jenkins) <no-reply@coreboot.org>
    (cherry picked from commit 2b1fdb034a895811283383c7fda1ecc3016f5a42)

--- src/soc/mediatek/mt8186/devapc.c
+++ src/soc/mediatek/mt8186/devapc.c
@@ -1325,6 +1325,7 @@ void dapc_init(void)
 
 		/* Init dapc */
 		write32(getreg(devapc_ao_base, AO_APC_CON), 0x0);
+		write32(getreg(devapc_ao_base, AO_APC_CON), 0x1);
 
 		/* Initialization */
 		if (devapc_init[i].init)
